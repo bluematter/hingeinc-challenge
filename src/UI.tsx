db006575a900e79f0e3fd3b88f72940b3f846eff
michael aubry <michaelaubry1991@gmail.com>
2018-06-21 16:40:00 -0700

    Added some media queries

--- src/UI.tsx
+++ src/UI.tsx
@@ -10,7 +10,7 @@ class UI extends React.PureComponent<UIProps> {
     const { counter, loading } = this.props;
 
     return (
-      <div>
+      <div className="ui-overflow-wrap">
         <div className="eightys-text">{counter}</div>
         {loading && (
           <div className="eightys-text eightys-text-loading">Loading...</div>
